5b80eceb354af3daec5381b968a4278241497797
Blaž Škufca <3877198+blazskufca@users.noreply.github.com>
2024-12-28 10:54:31 +0100

    [css] - Build CSS with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [],
+  content: [
+      "./assets/templates/**/*.tmpl",
+      "./assets/templates/errors/*.tmpl",
+      "./assets/templates/base/*.tmpl",
+      "./assets/static/js/*.js"
+  ],
   theme: {
     extend: {},
   },
